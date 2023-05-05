2316d5bd18c122b3e32fb69739bfaae3b5c25c65
zero-two <miigo@duck.com>
2023-05-06 01:07:05 +0700

    Dependency

--- src/main/java/franxx/code/maven/App.java
+++ src/main/java/franxx/code/maven/App.java
@@ -1,10 +1,16 @@
 package franxx.code.maven;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Gson gson = new Gson();
+        Person person = new Person("Hilmi Akbar Muharrom");
+
+        String json = gson.toJson(person);
+        System.out.println(json);
     }
 }
